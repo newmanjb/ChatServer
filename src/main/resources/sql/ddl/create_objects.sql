3d933913f0cfa81a4bab319c00c447531cd074ce
newmanjb <joshuajmnewman@gmail.com>
2024-10-10 09:17:13 +0100

    Added the "conversation_participant_id" concept and cleaned things up a bit as a result

--- src/main/resources/sql/ddl/create_objects.sql
+++ src/main/resources/sql/ddl/create_objects.sql
@@ -1,13 +1,21 @@
+drop table if exists conversations cascade;
+drop table if exists conversation_participants cascade;
+drop table if exists messages cascade;
+drop table if exists users cascade;
+drop table if exists drafted_messages cascade;
+
 create table users (
 user_id uuid, age integer not null, first_name varchar(40) not null, last_name varchar(50) not null, primary key(user_id)
 );
 
 create table conversations (conversation_id uuid, conversation_name varchar(50) not null, date_started timestamp not null, date_ended timestamp, primary key (conversation_id));
 
-create table drafted_messages (user_id uuid, conversation_id uuid, msg_text text, primary key (user_id, conversation_id), constraint fk_drafted_messages_users foreign key(user_id) references users(user_id), constraint fk_drafted_messages_conversations foreign key(conversation_id) references conversations(conversation_id));
+create table conversation_participants(conversation_id uuid, user_id uuid, participant_id uuid unique not null, primary key (conversation_id, user_id), constraint fk_conversation_participants_users foreign key(user_id) references users(user_id), constraint fk_conversation_participants_conversations foreign key(conversation_id) references conversations(conversation_id));
 
-create table conversation_participants(conversation_id uuid, user_id uuid, primary key (conversation_id, user_id), constraint fk_conversation_participants_conversations foreign key(conversation_id) references conversations(conversation_id), constraint fk_conversation_participants_users foreign key(user_id) references users(user_id));
+create table drafted_messages (participant_id uuid, msg_text text not null, primary key (participant_id), constraint fk_drafted_messages_conversation_participants foreign key(participant_id) references conversation_participants(participant_id));
 
-create table messages(message_id uuid, date_sent timestamp, sent_from uuid, msg_text text, primary key(message_id), constraint fk_messages_users foreign key(sent_from) references users(user_id));
 
-create table conversation_messages(conversation_id uuid, message_id uuid, primary key (conversation_id, message_id), constraint fk_conversation_messages_conversations foreign key(conversation_id) references conversations(conversation_id), constraint fk_conversation_messages_messages foreign key(message_id) references messages(message_id));
+create table messages(message_id uuid, date_sent timestamp not null, participant_id uuid not null, msg_text text not null, primary key(message_id), constraint fk_messages_conversation_participants foreign key(participant_id) references conversation_participants(participant_id));
+
+
+
